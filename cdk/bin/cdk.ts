1559c397e1db279b642793f6c9f4851d7ee2fc5c
Salvatore Cirone <48855938+aka-somix@users.noreply.github.com>
2024-11-13 22:29:51 +0100

    :sparkles: (cdk) introduce md5 in stack name to allow multiple deployments

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,9 +3,21 @@ import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
 import { EventsGateTinyStack, resPrefix } from '../lib/cdk-stack';
 import { tags } from '../lib/tags/.tags.json';
+import { createHash } from 'crypto';
+
+
+const stackNameGenerator = (app: cdk.App) => {
+    const eventbusname = app.node.tryGetContext('bus') ?? 'default';
+
+    const hash = createHash('md5');
+    hash.update(eventbusname);
+    const hashFromBus = hash.digest('hex').substring(0, 8);
+
+    return `EventsGateTinyStack${hashFromBus}`
+}
 
 const app = new cdk.App();
-new EventsGateTinyStack(app, 'EventsGateTinyStack', {});
+new EventsGateTinyStack(app, stackNameGenerator(app), {});
 
 // Statically tag the Project
 cdk.Tags.of(app).add("Project", resPrefix);
